95ea91794170d093a1a8ab55f69c303129bc6f5d
Anastasia Botsula <nactya.botsula@gmail.com>
2024-01-15 00:45:09 +0300

    feat: implement events when the virtual keyboard button is pressed

--- hangman/script.js
+++ hangman/script.js
@@ -33,7 +33,7 @@ gallowsSection.appendChild(gameName);
 
 function createHangmanPartsImgElement(imgName) {
   const hangmanPart = document.createElement('img');
-  hangmanPart.className = `hangman__${imgName}-img hangman-part-img`;
+  hangmanPart.className = `hangman__${imgName}-img hangman-part-img --hidden`;
   hangmanPart.src = `./images/${imgName}.svg`;
 
   hangman.appendChild(hangmanPart);
@@ -71,13 +71,30 @@ const keyboardLetters = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
 
+let pressedButton = null;
+
 function createKeyboard(letter) {
   const keyboardBtn = document.createElement('button');
   keyboardBtn.className = 'keyboard__btn btn';
   keyboardBtn.innerText = `${letter}`;
 
   keyboard.appendChild(keyboardBtn);
+
+  eventsOfClickedKeyboardButton(keyboardBtn);
 }
+
+function disablebButton(thisBtn) {
+  thisBtn.onclick = null;
+  thisBtn.classList.add('keyboard__btn_disabled');
+}
+
+function eventsOfClickedKeyboardButton(btn) {
+  btn.onclick = function() {
+    pressedButton = this.innerText;
+    checkLetter();
+    disablebButton(this);
+  };
+} 
 
 keyboardLetters.forEach(letter => createKeyboard(letter));
 
@@ -103,3 +120,26 @@ function showSecretWord(letter) {
 }
 
 word.split('').forEach(letter => showSecretWord(letter));
+
+let counter = 0;
+
+function checkLetter() {
+  for (const el of document.querySelectorAll('.word-letter')) {
+    if (el.textContent.includes(pressedButton)) {
+      el.nextSibling.classList.add('--hidden');
+    }
+  }
+
+  if (!word.includes(pressedButton)) {
+    counter++;
+    showHangmanPart();
+  }
+}
+
+// Hangman
+const hangmanPartsImg = document.querySelectorAll('.hangman-part-img');
+
+function showHangmanPart() {
+  const thisPart = hangmanPartsImg[counter - 1];
+  thisPart.classList.remove('--hidden');
+}
